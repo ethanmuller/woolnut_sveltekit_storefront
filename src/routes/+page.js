d16434f401ac0fc3f91390db3711249ce4830c16
Ethan Muller <ethanbmuller@gmail.com>
2024-09-24 09:58:51 -0400

    wip

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,13 @@
 import { products } from './products';
 /** @type {import('./$types').PageLoad} */
-export function load({ params }) {
-	return products;
+export async function load({ params }) {
+  const response = await fetch("http://localhost:9000/store/products")
+  try {
+    if (response.ok) {
+      const json = await response.json();
+      return json;
+    }
+  } catch (error) {
+    console.error(error.message)
+  }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,14 +10,14 @@
 
 <div class="c-catalog">
 	{#each data.products as product}
-	<a href="/products/{product.slug}">
-		<div class="bodoni-moda-regular c-listing">
-		<img class="c-listing__image" src="{product.image_url}" alt="" />
-		<div class="c-listing__text">
-			<div class="c-listing__name">{product.title}</div>
-			<div class="c-listing__price">${@html product.price_in_cents / 100}</div>
-		</div>
-		</div>
-	</a>
+    <a href="/products/{product.slug}">
+      <div class="bodoni-moda-regular c-listing">
+      <img class="c-listing__image" src="{product.image_url}" alt="" />
+      <div class="c-listing__text">
+        <div class="c-listing__name">{product.title}</div>
+        <div class="c-listing__price">${@html product.price_in_cents / 100}</div>
+      </div>
+      </div>
+    </a>
 	{/each}
 </div>
