4259548a87954ff2c73fb24fae3ae059192f1e52
Ethan Muller <ethanbmuller@gmail.com>
2024-09-25 14:49:07 -0400

    oops

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,9 +1,9 @@
 /** @type {import('@sveltejs/kit').HandleFetch} */
 export async function handleFetch({ request, fetch }) {
-	if (request.url.startsWith('https://api.yourapp.com/')) {
+	if (request.url.startsWith('https://woolnut-backend.mush.network')) {
 		// clone the original request, but change the URL
 		request = new Request(
-			request.url.replace('https://woolnut-backend.mush.network/', 'http://localhost:9000/'),
+			request.url.replace('https://woolnut-backend.mush.network', 'http://localhost:9000'),
 			request
 		);
 	}
