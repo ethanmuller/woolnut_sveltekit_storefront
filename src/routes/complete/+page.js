8d48033b603aae9d2c2209cefeefc31028290fb6
Ethan Muller <ethanbmuller@gmail.com>
2024-10-18 22:40:56 -0400

    oops

--- src/routes/complete/+page.js
+++ src/routes/complete/+page.js
@@ -5,7 +5,6 @@ const stripe = new Stripe(STRIPE_SECRET_KEY)
 export async function load({ url }) {
   const session_id = url.searchParams.get('session_id');
   const session = await stripe.checkout.sessions.retrieve(session_id);
-  console.log(session)
   
   return {
     session,
